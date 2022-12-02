4b53364fc4086499d26672d4c2fc00b37a002ed1
MohamedGouaouri <im_gouaouri@esi.dz>
2022-12-02 20:19:25 +0100

    commit

--- src/main/java/com/example/findmeadoctor/MainActivity.kt
+++ src/main/java/com/example/findmeadoctor/MainActivity.kt
@@ -10,8 +10,13 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.findmeadoctor.presentation.screens.LauncherScreen
+import com.example.findmeadoctor.presentation.screens.Routes
+import com.example.findmeadoctor.presentation.screens.device_details.DeviceDetails
+import com.example.findmeadoctor.presentation.screens.devices_list.Home
 import com.example.findmeadoctor.presentation.screens.energy_cost.EnergyCost
 import com.example.findmeadoctor.presentation.ui.theme.FindMeADoctorTheme
 import dagger.hilt.android.AndroidEntryPoint
